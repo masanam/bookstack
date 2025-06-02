4f9b59c03dc7a8702261c4cc33b9abb6d8587fb3
masanam <masanam@yahoo.com>
2025-06-02 10:36:26 +0700

    update logo white

--- resources/views/layouts/parts/header-logo.blade.php
+++ resources/views/layouts/parts/header-logo.blade.php
@@ -1,5 +1,5 @@
 <a href="{{ url('/') }}" data-shortcut="home_view" class="logo">
     @if(setting('app-logo', '') !== 'none')
-        <img class="logo-image" src="{{ setting('app-logo', '') === '' ? url('/logo.png') : url(setting('app-logo', '')) }}" alt="Logo">
+        <img class="logo-image" src="{{ setting('app-logo', '') === '' ? url('/white-logo.png') : url(setting('app-logo', '')) }}" alt="Logo">
     @endif
 </a>
